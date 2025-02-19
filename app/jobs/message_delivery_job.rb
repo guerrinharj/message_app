27a68d654ccaa124be843fd92bd97b0fd6d8d4e7
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-02-19 03:10:29 -0300

    created sidekiq conig

--- app/jobs/message_delivery_job.rb
+++ app/jobs/message_delivery_job.rb
@@ -1,12 +1,18 @@
 class MessageDeliveryJob < ApplicationJob
-    queue_as :messages
+    queue_as :default
+
+    sidekiq_options retry: false # Ensure job runs only once
 
     def perform(sender_id, receiver_id, content)
         sender = User.find(sender_id)
         receiver = User.find(receiver_id)
-    
+
         message = Message.create!(sender: sender, receiver: receiver, content: content)
-    
-        ChatChannel.broadcast_to("chat_#{receiver.id}", message.as_json)
+
+        # Ensure ActionCable is loaded before broadcasting
+        ActiveSupport::Dependencies.autoload_paths << Rails.root.join('app/channels')
+
+        # Broadcast the message via WebSocket
+        ActionCable.server.broadcast("chat_#{receiver.id}", message.as_json)
     end
 end
